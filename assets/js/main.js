b01babe6dcfd0a84d10d39b437d48e99420dc971
DanieleMorello <danielemorello@live.it>
2023-03-20 14:52:50 +0100

    empty count and add a condition of game over

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,15 +10,20 @@
 const paragraphEl = document.querySelector('p')
 
 // Aggiungo un contatore e lo setto a 0
-let count = 0;
+let count = 0
+
+// Aggiungo la variabile per gestire la fine del gioco
+let gameOver = false
 
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // Seleziono il bottone e lo salvo in una variabile
 const btnEL = document.querySelector('button')
-btnEL.addEventListener("click", function() {
+btnEL.addEventListener('click', function () {
   // Seleziono il container e lo salvo in una variabile
   const containerEL = document.querySelector('.container')
-  containerEL.innerHTML = ''
+  containerEL.innerHTML = ""
+  paragraphEl.innerHTML = ""
+  count = ""
 
   // Creo un max numero di celle
   let numCells = 101
@@ -32,7 +37,7 @@ btnEL.addEventListener("click", function() {
     containerEL.innerHTML += cell
   }
 
-  // Seleziono una cella 
+  // Seleziono una cella
   const cellEl = document.querySelectorAll('.cell')
   // Aggiungo eventlisner ciclando per la lunghezza dell'array cellEl
   for (let i = 0; i < cellEl.length; i++) {
@@ -40,21 +45,28 @@ btnEL.addEventListener("click", function() {
     console.log(thisCell)
 
     thisCell.addEventListener('click', function() {
-       /*
+      /*
         Se il numero è presente nella lista dei numeri generati
         abbiamo calpestato una bomba:
         -la cella si colora di rosso e la partita termina.
       */
-      if(thisCell[i] === bomb[i]) {
-        thisCell.style.backgroundColor = 'red'
-        thisCell.innerHTML = '💣'
-        console.log(`Boom`);
-        paragraphEl.innerHTML = `Hai perso!!! Celle cliccate: ${count}`
+      if (thisCell[i] === bomb[i]) {
+        thisCell.style.backgroundColor = "red"
+        thisCell.innerHTML = "💣"
+        console.log('Boom')
+        // Imposto gameOver a true
+        gameOver = true
+        return
       } else {
         thisCell.classList.toggle('bg_blue')
         thisCell.classList.toggle('active')
         console.log(`Cella cliccata: ${thisCell.textContent}`)
-        count++;
+        count++
+      }
+
+      if(gameOver = true) {
+        paragraphEl.innerHTML = `Hai perso!!! Celle cliccate: ${count}`
+        /* thisCell.removeEventListener('click', function () { }) */
       }
     });
   }
@@ -81,23 +93,23 @@ l’utente ha cliccato su una cella che non era una bomba.
 //Il computer deve generare 16 numeri casuali
 function randomNumbers(numMax) {
   // Creo un array vuoto per contenere i numeri casuali
-const randomNumbers = []
+  const randomNumbers = []
 
-// Genero un numero casuale compreso tra 1 e 100
-// let randomNumber = Math.floor(Math.random() * 100) + 1;
-
-// Ciclo il numero casuale
-for (let i = 0; randomNumbers.length < numMax; i++) {
   // Genero un numero casuale compreso tra 1 e 100
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  // let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-  // Verifico se il numero casuale non è già presente nell'array
-  if (randomNumbers.indexOf(randomNumber) === -1) {
-    // Aggiungo il numero casuale all'array
-    randomNumbers.push(randomNumber)
+  // Ciclo il numero casuale
+  for (let i = 0; randomNumbers.length < numMax; i++) {
+    // Genero un numero casuale compreso tra 1 e 100
+    const randomNumber = Math.floor(Math.random() * 100) + 1
+
+    // Verifico se il numero casuale non è già presente nell'array
+    if (randomNumbers.indexOf(randomNumber) === -1) {
+      // Aggiungo il numero casuale all'array
+      randomNumbers.push(randomNumber)
+    }
   }
-}
 
-// Stampo l'array di numeri casuali
+  // Stampo l'array di numeri casuali
   return randomNumbers
 }
