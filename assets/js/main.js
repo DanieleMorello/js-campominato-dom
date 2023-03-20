a42c8aef258215512271ba6bf1b445ab0e9fd715
DanieleMorello <danielemorello@live.it>
2023-03-20 22:36:06 +0100

    Change padding a p, change position a label

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,81 +7,82 @@
  */
 
 // Seleziono P
-const paragraphEl = document.querySelector('p')
+const paragraphEl = document.querySelector("p");
 
 // Aggiungo un contatore e lo setto a 0
-let count = 0
+let count = 0;
 
 // Aggiungo la variabile per gestire la fine del gioco
-let gameOver = false
+let gameOver = false;
 
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // Seleziono il bottone e lo salvo in una variabile
-const btnEL = document.querySelector('button')
-btnEL.addEventListener('click', function () {
+const btnEL = document.querySelector("button");
+btnEL.addEventListener("click", function () {
   // Seleziono il container e lo salvo in una variabile
-  const containerEL = document.querySelector('.container')
-  containerEL.innerHTML = ""
-  paragraphEl.innerHTML = ""
-  count = ""
+  const containerEL = document.querySelector(".container");
+  containerEL.innerHTML = "";
+  paragraphEl.innerHTML = "";
+  count = "";
 
   // Creo un max numero di celle
-  let numCells = 101
+  let numCells = 101;
 
-  const bomb = randomNumbers(16)
-  console.log(bomb)
+  const bomb = randomNumbers(16);
+  console.log(bomb);
 
   // Creo un ciclo n volte per le celle
   for (let i = 1; i < numCells; i++) {
-    const cell = `<div class="cell">${i}</div>`
-    containerEL.innerHTML += cell
+    const cell = `<div class="cell">${i}</div>`;
+    containerEL.innerHTML += cell;
 
-  // Seleziono la select e la salvo in una variabile
-  // const selectEl = document.querySelector('select')
-  // selectEl.addEventListener('submit', function() {
-  //   if(selectEl.value === 'normal') {
-  //     numCells = 81
-  //     containerEL.innerHTML += cell.add.classList('normal')
-  //   } else if (selectEl.value === 'hard') {
-  //     numCells = 49
-  //     containerEL.innerHTML += cell.add.classList('hard')
-  //   }
-  // })
-}
+    // Seleziono la select e la salvo in una variabile
+    const selectEl = document.querySelector("select");
+    selectEl.addEventListener("submit", function () {
+      if (selectEl.value === "normal") {
+        numCells = 81;
+        containerEL.innerHTML += cell.add.classList("normal");
+      } else if (selectEl.value === "hard") {
+        numCells = 49;
+        containerEL.innerHTML += cell.add.classList("hard");
+      }
+    });
+  }
 
   // Seleziono una cella
-  const cellEl = document.querySelectorAll('.cell')
+  const cellEl = document.querySelectorAll(".cell");
   // Aggiungo eventlisner ciclando per la lunghezza dell'array cellEl
   for (let i = 0; i < cellEl.length; i++) {
-    const thisCell = cellEl[i]
-    console.log(thisCell)
+    const thisCell = cellEl[i];
+    // console.log(thisCell)
 
-    thisCell.addEventListener('click', function() {
+    thisCell.addEventListener("click", function () {
       /*
         Se il numero è presente nella lista dei numeri generati
         abbiamo calpestato una bomba:
         -la cella si colora di rosso e la partita termina.
       */
-        if(gameOver !== true) {
-          
-          if (thisCell[i] === bomb[i]) {
-            thisCell.style.backgroundColor = "red"
-            thisCell.innerHTML = `<i class="fa fa-bomb" aria-hidden="true"></i>`
-            console.log('Boom')
-            
-            // Imposto gameOver a true
-            gameOver = true
-            if(gameOver === true) {
-              paragraphEl.innerHTML = `<span>Hai perso!!! Ricarica la pagina per ricominciare</span><br> Celle cliccate: ${count}`
-            }
+      if (gameOver !== true) {
+        if (thisCell[i] === bomb[i]) {
+          thisCell.style.backgroundColor = "red";
+          thisCell.innerHTML = `<i class="fa fa-bomb" aria-hidden="true"></i>`;
+          console.log("Boom");
 
-          } else {
-            thisCell.classList.toggle('bg_blue')
-            thisCell.classList.toggle('active')
-            console.log(`Cella cliccata: ${thisCell.textContent}`)
-            count++
+          // Imposto gameOver a true
+          gameOver = true;
+          if (gameOver === true) {
+            paragraphEl.innerHTML = `<span>Hai perso!!! Ricarica la pagina per ricominciare</span><br> Celle cliccate: ${count}`;
+          } else if (count === numCells - numMax) {
+            gameOver = true;
+            paragraphEl.innerHTML = "Hai vinto! Bravissimo!";
           }
+        } else {
+          thisCell.classList.toggle("bg_blue");
+          thisCell.classList.toggle("active");
+          console.log(`Cella cliccata: ${thisCell.textContent}`);
+          count++;
         }
+      }
     });
   }
 });
@@ -107,7 +108,7 @@ l’utente ha cliccato su una cella che non era una bomba.
 //Il computer deve generare 16 numeri casuali
 function randomNumbers(numMax) {
   // Creo un array vuoto per contenere i numeri casuali
-  const randomNumbers = []
+  const randomNumbers = [];
 
   // Genero un numero casuale compreso tra 1 e 100
   // let randomNumber = Math.floor(Math.random() * 100) + 1;
@@ -115,15 +116,15 @@ function randomNumbers(numMax) {
   // Ciclo il numero casuale
   for (let i = 0; randomNumbers.length < numMax; i++) {
     // Genero un numero casuale compreso tra 1 e 100
-    const randomNumber = Math.floor(Math.random() * 100) + 1
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
 
     // Verifico se il numero casuale non è già presente nell'array
     if (randomNumbers.indexOf(randomNumber) === -1) {
       // Aggiungo il numero casuale all'array
-      randomNumbers.push(randomNumber)
+      randomNumbers.push(randomNumber);
     }
   }
 
   // Stampo l'array di numeri casuali
-  return randomNumbers
+  return randomNumbers;
 }
